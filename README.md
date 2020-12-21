4119620201185f999e03e6177bceaf77990e554a
defnngj <defnngj@gmail.com>
2020-12-22 00:52:32 +0800

    interface case

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # django_celery
+
+
+## 运行方式
+
+1.启动 redis 
+
+```shell script
+> redis-server
+```
+
+2.启动 celery （进入`django_celery` 项目目录运行）
+```shell script
+> celery -A django_celery worker --loglevel=info
+```
+
+3.启动django
+
+```shell script
+> python .\manage.py runserver
+```
--- api/cases/test_case.py
+++ api/cases/test_case.py
@@ -1,18 +1,32 @@
 import unittest
 import requests
-from celery import shared_task
-from seldom import data
-import xmlrunner
+from parameterized import parameterized
 
 
-class Data:
-    data_list = []
+def interface_data():
+    """
+    定义接口数据
+    """
+    data_list = [
+        ("http://httpbin.org/get", "get", {"params": {'key': 'value'} }, "resp"),
+        ("http://httpbin.org/post", "post", {"data":{'key': 'value'}}, "resp"),
+    ]
+    return data_list
 
 
 class Test(unittest.TestCase):
-
-    @data(["1", "2"])
-    def test_case(self, number):
-        print("-->", number)
-        r = requests.get('https://api.github.com/events')
-        self.assertEqual(r.status_code, 200)
+    """
+    接口测试用例
+    """
+    @parameterized.expand(interface_data)
+    def test_case(self, url, method, params, resp):
+        print("url", url)
+        print("-->", method)
+        print("-->", params)
+        print("-->", resp)
+        if method == "get":
+            r = requests.get(url, params=params)
+            self.assertEqual(r.status_code, 200)
+        if method == "post":
+            r = requests.post(url, data=params)
+            self.assertEqual(r.status_code, 200)
